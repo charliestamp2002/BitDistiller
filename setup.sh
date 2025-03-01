6be30f0e222f6eb6aa75ce0013c38a1dba9c1f27
BrownianNotion <andrewwusyd@gmail.com>
2025-03-01 14:52:57 +0000

    fix bug in setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install -y python3.9 python3.9-venv
+sudo apt install -y python3.9 python3.9-venv python3.9-dev
 python3.9 -m venv BitDistillerVenv
 source BitDistillerVenv/bin/activate
 pip install -r requirements.txt
