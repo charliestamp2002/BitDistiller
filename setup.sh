aaee8635a77272779ace958465d949d929abd320
BrownianNotion <andrewwusyd@gmail.com>
2025-03-02 18:53:20 +0000

    Fix requirements.txt

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
+# Run first to set up virtual environment and correct packages
+# should take few min
 sudo apt update
 sudo apt install -y python3.9 python3.9-venv python3.9-dev
 python3.9 -m venv BitDistillerVenv
 source BitDistillerVenv/bin/activate
 pip install -r requirements.txt
-
